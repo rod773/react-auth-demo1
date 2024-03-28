adae228858b99314f38513dc432d3c4116bd7a14
rod773 <104315743+rod773@users.noreply.github.com>
2024-03-28 19:46:43 +0100

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,11 @@
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
+  const navigate = useNavigate();
+  const handleEntrar = () => {
+    navigate("/login");
+  };
+
   return (
     <div>
       <nav className="fixed inset-x-0 top-0 z-10 w-full px-4 py-1 bg-white shadow-md border-slate-500 dark:bg-[#0c1015] transition duration-700 ease-out">
@@ -7,8 +14,11 @@ const Home = () => {
             Logo
           </div>
           <div className="flex items-center space-x-4 text-lg font-semibold tracking-tight">
-            <button className="px-6 py-2 text-black transition duration-700 ease-out bg-white border border-black rounded-lg hover:bg-black hover:border hover:text-white dark:border-white dark:bg-inherit dark:text-white dark:hover:bg-white dark:hover:text-black">
-              Login
+            <button
+              onClick={handleEntrar}
+              className="px-6 py-2 text-black transition duration-700 ease-out bg-white border border-black rounded-lg hover:bg-black hover:border hover:text-white dark:border-white dark:bg-inherit dark:text-white dark:hover:bg-white dark:hover:text-black"
+            >
+              Entrar
             </button>
             <button className="px-6 py-2 text-white transition duration-500 ease-out bg-blue-700 rounded-lg hover:bg-blue-800 hover:ease-in hover:underline">
               Registro
