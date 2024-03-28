8cc868ff4ea6443ae5ac88a219e8f2c09b7b2756
rod773 <rodrigoarcadio1@gmail.com>
2024-03-28 10:19:28 +0100

    login component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import AuthProvider from "./provider/authProvider";
-import Routes from "./routes";
+import AuthProvider from "provider/authProvider";
+import Routes from "routes";
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Routes />
+    </AuthProvider>
   );
 }
 
