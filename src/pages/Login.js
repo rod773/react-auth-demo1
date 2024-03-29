05c4d5b8660135dfd21bfd5811a5547e7f7da9f8
rod773 <rodrigoarcadio1@gmail.com>
2024-03-29 09:58:14 +0100

    update

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,18 +1,44 @@
 import { useAuth } from "provider/authProvider";
-import { useNavigate } from "react-router-dom";
+import { Form, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 const Login = () => {
+  const url = "https://infodemencias.com/wp-json/trabajadores/v1/login";
+
   const { setToken } = useAuth();
   const navigate = useNavigate();
 
-  const handleLogin = () => {
-    //setToken("this is a test token");
-    // navigate("/", { replace: true });
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const getToken = async () => {
+    const config = {
+      url: url,
+      method: "POST",
+      headers: {
+        "Content-type": "application/json;charset=UTF-8",
+      },
+      data: JSON.stringify(formData),
+    };
+
+    await axios(config)
+      .then((response) => {
+        //console.log(response.data);
+        setToken(response.data.token);
+      })
+      .catch((error) => console.log(error));
   };
 
-  // setTimeout(() => {
-  //   handleLogin();
-  // }, 3 * 1000);
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    getToken();
+
+    navigate("/", { replace: true });
+  };
 
   return (
     <>
@@ -29,6 +55,10 @@ const Login = () => {
                     type="text"
                     placeholder="Email"
                     className=" border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-1 focus:ring-indigo-600 rounded-md"
+                    value={formData.email}
+                    onChange={(e) =>
+                      setFormData({ ...formData, email: e.target.value })
+                    }
                   />
                   <label className="block mt-3 font-semibold">
                     Password
@@ -37,6 +67,10 @@ const Login = () => {
                         type="password"
                         placeholder="Password"
                         className=" border w-full h-5 px-3 py-5 mt-2 hover:outline-none focus:outline-none focus:ring-1 focus:ring-indigo-600 rounded-md"
+                        value={formData.password}
+                        onChange={(e) =>
+                          setFormData({ ...formData, password: e.target.value })
+                        }
                       />
                       <div className="flex justify-between items-baseline">
                         <button
